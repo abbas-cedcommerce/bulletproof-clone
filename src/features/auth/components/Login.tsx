4c4a893a4862c9327c48a74fa39843b3bb057cb6
abbas-cedcommerce <Abbas-CedCommerce>
2023-06-26 18:12:22 +0530

    Login form fixed

--- src/features/auth/components/Login.tsx
+++ src/features/auth/components/Login.tsx
@@ -1,8 +1,19 @@
 import { Button, Card, FlexLayout, FormElement, TextField, TextStyles } from '@cedcommerce/ounce-ui';
-import React from 'react'
+import React, { useState } from 'react'
 import AuthLayout from '../design-templates/auth-design-templaets';
 
 export function Login() {
+
+    const [fieldValues, setInputs] = useState({
+        email: '', password: ""
+    });
+
+    function handleInputs(key: string, value: string) {
+        let newInput: any = { ...fieldValues };
+        newInput[key] = value;
+        setInputs(newInput);
+    }
+
     return (
         <AuthLayout>
             <Card cardType="Shadowed">
@@ -14,22 +25,22 @@ export function Login() {
                         <TextField
                             // error={fieldErrors?.email?.value}
                             name={"Email"}
-                            // onChange={(e) => { handleInputs("email", e) }}
+                            onChange={(e) => { handleInputs("email", e) }}
                             placeHolder="Enter email address or username"
                             required={true}
                             // showHelp={fieldErrors.email.value ? fieldErrors.email.message : ''}
                             type="text"
-                        // value={fieldValues.email}
+                            value={fieldValues.email}
                         />
                         <TextField
                             // error={fieldErrors?.password?.value}
                             name={"Password"}
-                            // onChange={(e) => { handleInputs("password", e) }}
+                            onChange={(e) => { handleInputs("password", e) }}
                             placeHolder="Enter Password"
                             required={true}
                             // showHelp={fieldErrors.password.value ? fieldErrors.password.message : ''}
                             type="password"
-                        // value={fieldValues.password}
+                            value={fieldValues.password}
                         />
                         <hr className="custom-hr" />
                         <Button
